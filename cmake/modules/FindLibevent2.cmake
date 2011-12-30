a942b5baa5af8da32bfe37f1b46822cf3d26763f
jonb <jonb@lanl.gov>
2011-12-30 11:36:55 -0700

    Addition of paths to support libraries included in macports.

--- cmake/modules/FindLibevent2.cmake
+++ cmake/modules/FindLibevent2.cmake
@@ -8,6 +8,7 @@ else (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
             ${LIBEVENT2_PREFIX}/include
             /usr/include
             /usr/local/include
+            /opt/local/include
         NO_DEFAULT_PATH
     )
 
@@ -18,6 +19,7 @@ else (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
             ${LIBEVENT2_PREFIX}/lib
             /usr/lib
             /usr/local/lib
+            /opt/local/lib
         NO_DEFAULT_PATH
     )
     if(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
