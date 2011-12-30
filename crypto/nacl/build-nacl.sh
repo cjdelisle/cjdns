24f524a1883c7fb8804e36041039b710ca051bc4
cjdelisle <calebdelisle@lavabit.com>
2011-12-30 06:14:23 -0500

    improved build-nacl.sh

--- crypto/nacl/build-nacl.sh
+++ crypto/nacl/build-nacl.sh
@@ -8,7 +8,21 @@ rm -f ./do.loudly
 
 mv ./build ../../../build/nacl-build
 cd ../../../build/nacl-build
+
 mkdir include
-find ./ -name '*.h' -exec mv {} ./include/ \;
 mkdir lib
-find ./ -name '*.a' -exec mv {} ./lib/ \;
+
+ARCH=`uname -m`
+HOST=`uname -n`
+SDIR=''
+
+case $ARCH in
+    x86_64) [[ -d "$HOST/include/amd64" ]] && SDIR="amd64"
+	;;
+    i686) [[ -d "$HOST/include/x86" ]] && SDIR="x86"
+	;;
+esac
+
+mv $HOST/include/*.h ./include/
+find ./$HOST/include/$SDIR -name '*.h' -exec mv {} ./include/ \;
+find ./$HOST/lib/$SDIR -name '*.a' -exec mv {} ./lib/ \;
