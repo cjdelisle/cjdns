4c92b03b424e2f156c8280074fe1d1f0d96e2595
jonb <jonb@lanl.gov>
2012-01-25 08:56:17 -0700

    Fixed up array counters.

--- scripts/darwin_tun.sh
+++ scripts/darwin_tun.sh
@@ -49,18 +49,22 @@ avail_tun=(
 )
 
 # Search for opened tun devices and exclude them from the list.
-avail_tun_size=${#avail_tun[@]}
-avail_tun_max=$((avail_tun_size - 1))
+orig_tun_count=${#avail_tun[@]}
+orig_tun_max=$((orig_tun_count - 1))
 for i in $(lsof -Fn | tail -n +2 | cut -c2- | grep "/dev/tun" | sort -u | uniq ) ; do
-    for ((j = 0; j <= avail_tun_max; j++)); do
+    for ((j = 0; j <= orig_tun_max; j++)); do
         if [ $(echo "$i" | grep "${avail_tun[j]}") ]; then
             unset avail_tun[$j]
         fi
     done
 done
 
+# Update length
+avail_tun_size=${#avail_tun[@]}
+avail_tun_max=$((avail_tun_size - 1))
+
 # Pick a random tun device from the available ones.
-tun_dev=${avail_tun[$[ ( $RANDOM % 15 ) + 0 ]]}
+tun_dev=${avail_tun[$[ ( $RANDOM % $avail_tun_max ) + 0 ]]}
 
 if ! [ $tun_dev ] ; then
     echo "DARWIN_TUN_NONE_AVAILABLE"
