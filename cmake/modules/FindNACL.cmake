7e7b35eca743a34084f84fb1224b4c8c700643e4
cjdelisle <calebdelisle@lavabit.com>
2012-01-28 22:09:09 -0500

    ExternalProject.cmake requires >=2.8, sorry guys, you're just going to have to upgrade :(

--- cmake/modules/FindNACL.cmake
+++ cmake/modules/FindNACL.cmake
@@ -9,6 +9,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+cmake_minimum_required(VERSION 2.8)
+
 if (NOT NACL_INCLUDE_DIRS)
     include(ExternalProject)
 
