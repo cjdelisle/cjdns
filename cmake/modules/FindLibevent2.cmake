226d14a7118ca8d20c1d193268b7947deeec94bb
cjdelisle <calebdelisle@lavabit.com>
2012-02-01 12:24:10 -0500

    Got static builds to work \o/

--- cmake/modules/FindLibevent2.cmake
+++ cmake/modules/FindLibevent2.cmake
@@ -9,63 +9,79 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-if (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
-    set(LIBEVENT2_FOUND TRUE)
-else (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
-    find_path(LIBEVENT2_INCLUDE_DIRS
-        NAMES
-            event2/dns.h
-        PATHS
-            ${LIBEVENT2_PREFIX}/include
-            /usr/include
-            /usr/local/include
-            /opt/local/include
-            ${CMAKE_BINARY_DIR}/libevent2-bin/include
-        NO_DEFAULT_PATH
-    )
+if ("$ENV{STATIC}" STREQUAL "")
+    if (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+        set(LIBEVENT2_FOUND TRUE)
+    else (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+        find_path(LIBEVENT2_INCLUDE_DIRS
+            NAMES
+                event2/dns.h
+            PATHS
+                ${LIBEVENT2_PREFIX}/include
+                /usr/include
+                /usr/local/include
+                /opt/local/include
+                ${CMAKE_BINARY_DIR}/libevent2-bin/include
+            NO_DEFAULT_PATH
+        )
 
-    find_library(LIBEVENT2_LIBRARIES
-        NAMES
-            event
-        PATHS
-            ${LIBEVENT2_INCLUDE_DIRS}/../lib
-            ${LIBEVENT2_PREFIX}/lib
-            /usr/lib
-            /usr/local/lib
-            /opt/local/lib
-        NO_DEFAULT_PATH
-    )
+        find_library(LIBEVENT2_LIBRARIES
+            NAMES
+                event
+            PATHS
+                ${LIBEVENT2_INCLUDE_DIRS}/../lib
+                ${LIBEVENT2_PREFIX}/lib
+                /usr/lib
+                /usr/local/lib
+                /opt/local/lib
+            NO_DEFAULT_PATH
+        )
+        if(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+           set(LIBEVENT2_FOUND TRUE)
+        endif(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+    endif (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+endif ("$ENV{STATIC}" STREQUAL "")
 
-    if(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
-       set(LIBEVENT2_FOUND TRUE)
-    endif(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
-endif (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
-
-if (NOT LIBEVENT2_FOUND)
+if (NOT LIBEVENT2_FOUND AND "$ENV{NO_STATIC}" STREQUAL "")
     include(ExternalProject)
 
     # Without this, the build doesn't happen until link time.
-    include_directories(${NACL_USE_FILES})
+    include_directories(${LIBEVENT2_USE_FILES})
+
+    # Need librt to be included if libevent is static linked.
+    find_package(Librt REQUIRED)
 
     ExternalProject_Add(Libevent2
-        GIT_REPOSITORY git://github.com/libevent/libevent.git
-        GIT_TAG 5de3fa3208c6d6221f188b623fb4b78992fda89a
+        URL "http://cloud.github.com/downloads/libevent/libevent/libevent-2.0.16-stable.tar.gz"
+        URL_MD5 "899efcffccdb3d5111419df76e7dc8df"
         SOURCE_DIR "${CMAKE_BINARY_DIR}/libevent2"
         BINARY_DIR "${CMAKE_BINARY_DIR}/libevent2-build"
-        CONFIGURE_COMMAND "${CMAKE_BINARY_DIR}/libevent2/configure
-            --prefix=/home/user/src/libevent-bin
+        CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/libevent2/configure
+            --prefix=${CMAKE_BINARY_DIR}/libevent2-bin
             --disable-openssl
             --disable-shared
             --with-pic
             --disable-thread-support
-        "
-        COMPILE_COMMAND "make"
-        INSTALL_COMMAND "make install"
+        BUILD_COMMAND make
+        TEST_COMMAND ""
+        INSTALL_COMMAND make install
         UPDATE_COMMAND ""
         PATCH_COMMAND ""
     )
 
     set(LIBEVENT2_INCLUDE_DIRS "${CMAKE_BINARY_DIR}/libevent2-bin/include/")
-    set(LIBEVENT2_LIBRARIES    "${CMAKE_BINARY_DIR}/libevent2-bin/lib/libevent.a")
 
-endif (LIBEVENT2_FOUND)
+    # This is needed to make sure libevent2 pulls in rt as a dependency.
+    add_library(event2 STATIC IMPORTED)
+    set_target_properties(event2 PROPERTIES
+        IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/libevent2-bin/lib/libevent.a
+        IMPORTED_LINK_INTERFACE_LIBRARIES ${LIBRT_LIBRARIES}
+    )
+
+    set(LIBEVENT2_LIBRARIES event2)
+
+endif (NOT LIBEVENT2_FOUND AND "$ENV{NO_STATIC}" STREQUAL "")
+
+if(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+   set(LIBEVENT2_FOUND TRUE)
+endif(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
