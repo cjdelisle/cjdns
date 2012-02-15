2b2940bc8dc3144da35d22ad8f74b89dda594ac8
cjdelisle <calebdelisle@lavabit.com>
2012-02-15 18:02:31 -0500

    Removed redundant buffering code from FileReader and added a test.

--- io/FileReader.c
+++ io/FileReader.c
@@ -12,27 +12,55 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "io/Reader.h"
+#include "io/FileReader.h"
 #include "memory/Allocator.h"
 
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
-
-#define BUFFER_SIZE 1024
 
 struct Context {
     FILE* toRead;
-    char buffer[BUFFER_SIZE];
-    char* pointer;
-    char* endPointer;
-    bool skipFailed;
+    bool failed;
     size_t bytesRead;
     struct Reader reader;
 };
 
-static int read(void* readInto, size_t length, const struct Reader* reader);
-static void skip(size_t byteCount, const struct Reader* reader);
-static size_t bytesRead(const struct Reader* reader);
+/** @see Reader->read() */
+static int read(void* readInto, size_t length, const struct Reader* reader)
+{
+    struct Context* context = (struct Context*) reader->context;
+    if (context->failed || fread((char*)readInto, 1, length, context->toRead) != length) {
+        context->failed = true;
+        return -1;
+    }
+    context->bytesRead += length;
+    return 0;
+}
+
+/** @see Reader->bytesRead() */
+static size_t bytesRead(const struct Reader* reader)
+{
+    // ftell() is unreliable.
+    return ((struct Context*) reader->context)->bytesRead;
+}
+
+/** @see Reader->skip() */
+static void skip(size_t length, const struct Reader* reader)
+{
+    struct Context* context = (struct Context*) reader->context;
+
+    #define BUFF_SZ 256
+    uint8_t buff[BUFF_SZ];
+
+    // fseek() and ftell() are unreliable.
+    size_t amount;
+    while ((amount = (length > BUFF_SZ) ? BUFF_SZ : length) && !context->failed) {
+        context->failed = read(buff, amount, reader);
+        length -= amount;
+    }
+}
 
 /** @see FileReader.h */
 struct Reader* FileReader_new(FILE* toRead, const struct Allocator* allocator)
@@ -40,8 +68,6 @@ struct Reader* FileReader_new(FILE* toRead, const struct Allocator* allocator)
     struct Context* context = allocator->calloc(sizeof(struct Context), 1, allocator);
 
     context->toRead = toRead;
-    context->pointer = context->buffer;
-    context->endPointer = context->buffer + fread(context->buffer, 1, BUFFER_SIZE, toRead);
 
     struct Reader localReader = {
         .context = context,
@@ -52,78 +78,4 @@ struct Reader* FileReader_new(FILE* toRead, const struct Allocator* allocator)
     memcpy(&context->reader, &localReader, sizeof(struct Reader));
 
     return &context->reader;
-}
-
-/** @see Reader->read() */
-static int read(void* readInto, size_t length, const struct Reader* reader)
-{
-    struct Context* context = (struct Context*) reader->context;
-    context->bytesRead += length;
-
-    if (length == 0) {
-        int ret = read(readInto, 1, reader);
-        context->pointer--;
-        return ret;
-    }
-
-    if (context->pointer + length > context->endPointer) {
-        size_t firstRead = context->endPointer - context->pointer;
-        if (firstRead > 0) {
-            read(readInto, firstRead, reader);
-        }
-        if (length - firstRead >= BUFFER_SIZE) {
-            size_t r = fread((char*)readInto + firstRead, 1, length - firstRead, context->toRead);
-            if (r < length - firstRead) {
-                return -1;
-            }
-        }
-        context->pointer = context->buffer;
-        context->endPointer =
-            context->buffer + fread(context->buffer, 1, BUFFER_SIZE, context->toRead);
-        if (length - firstRead >= BUFFER_SIZE) {
-            return 0;
-        }
-        if (context->pointer == context->endPointer) {
-            // EOF
-            return -1;
-        }
-        if (length - firstRead < BUFFER_SIZE) {
-            return read((char*)readInto + firstRead, length - firstRead, reader);
-        }
-    }
-
-    // Check for integer overflow
-    if (context->pointer + length < context->pointer) {
-        return -2;
-    }
-
-    memcpy(readInto, context->pointer, length);
-    context->pointer += length;
-
-    return 0;
-}
-
-/** @see Reader->bytesRead() */
-static size_t bytesRead(const struct Reader* reader)
-{
-    return ((struct Context*) reader->context)->bytesRead;
-}
-
-/** @see Reader->skip() */
-static void skip(size_t length, const struct Reader* reader)
-{
-    struct Context* context = (struct Context*) reader->context;
-    context->bytesRead += length;
-
-    while (context->pointer + length > context->endPointer) {
-        length -= (context->endPointer - context->pointer + 1);
-        char* discard;
-        context->pointer = context->endPointer;
-        if (read(&discard, 1, reader)) {
-            context->skipFailed = true;
-            return;
-        }
-    }
-
-    context->pointer += length;
 }
