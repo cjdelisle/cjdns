f0ae8c63b5de57fb473b58f1ff8de68e0bd46023
Caleb James DeLisle <calebdelisle@lavabit.com>
2012-03-09 23:19:10 -0500

    #39 Librt not always pulled in when libvevent is build statically.

--- cmake/modules/FindLibevent2.cmake
+++ cmake/modules/FindLibevent2.cmake
@@ -9,6 +9,18 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Need librt to be included if libevent is static linked.
+function(includeLibrt)
+    # Apple includes librt in the standard system library.
+    if(NOT DEFINED APPLE)
+        find_package(Librt REQUIRED)
+
+        set_property(TARGET event2
+            PROPERTY IMPORTED_LINK_INTERFACE_LIBRARIES ${LIBRT_LIBRARIES})
+    endif()
+endfunction()
+
 if ("$ENV{STATIC}" STREQUAL "")
     if (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
         set(LIBEVENT2_FOUND TRUE)
@@ -36,9 +48,13 @@ if ("$ENV{STATIC}" STREQUAL "")
                 /opt/local/lib
             NO_DEFAULT_PATH
         )
-        if(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
-           set(LIBEVENT2_FOUND TRUE)
-        endif(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+
+        if("${LIBEVENT2_INCLUDE_DIRS}" STREQUAL "${CMAKE_BINARY_DIR}/libevent2-bin/include")
+            add_library(event2 STATIC IMPORTED)
+            set_property(TARGET event2 PROPERTY IMPORTED_LOCATION ${LIBEVENT2_LIBRARIES})
+            includeLibrt()
+        endif()
+
     endif (LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
 endif ("$ENV{STATIC}" STREQUAL "")
 
@@ -47,12 +63,6 @@ if (NOT LIBEVENT2_FOUND AND "$ENV{NO_STATIC}" STREQUAL "")
 
     # Without this, the build doesn't happen until link time.
     include_directories(${LIBEVENT2_USE_FILES})
-
-    # Librt functions are in the standard library on apple.
-    if(NOT DEFINED APPLE)
-        # Need librt to be included if libevent is static linked.
-        find_package(Librt REQUIRED)
-    endif()
 
     ExternalProject_Add(Libevent2
         URL "http://cloud.github.com/downloads/libevent/libevent/libevent-2.0.16-stable.tar.gz"
@@ -87,15 +97,11 @@ if (NOT LIBEVENT2_FOUND AND "$ENV{NO_STATIC}" STREQUAL "")
     set_property(TARGET event2
         PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/libevent2-bin/lib/libevent.a)
 
-    if(NOT DEFINED APPLE)
-        # This is needed to make sure libevent2 pulls in rt as a dependency.
-        set(LIBEVENT2_LIBRARIES event2 ${LIBRT_LIBRARIES})
-    else()
-        set(LIBEVENT2_LIBRARIES event2)
-    endif()
+    includeLibrt()
 
-endif (NOT LIBEVENT2_FOUND AND "$ENV{NO_STATIC}" STREQUAL "")
+endif()
 
-if(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+if(NOT LIBEVENT2_FOUND AND LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+   set(LIBEVENT2_LIBRARIES event2)
    set(LIBEVENT2_FOUND TRUE)
-endif(LIBEVENT2_INCLUDE_DIRS AND LIBEVENT2_LIBRARIES)
+endif()
