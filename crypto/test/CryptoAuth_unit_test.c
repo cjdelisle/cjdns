a02dc3caa4df53e3f7b386e651a23bb5c1d7548c
Caleb James DeLisle <calebdelisle@lavabit.com>
2012-03-11 05:21:37 -0400

    Improved CryptoAuth unit tests.

--- crypto/test/CryptoAuth_unit_test.c
+++ crypto/test/CryptoAuth_unit_test.c
@@ -17,10 +17,13 @@
 #include "crypto/test/Exports.h"
 #include "io/FileWriter.h"
 #include "memory/BufferAllocator.h"
+#include "memory/MallocAllocator.h"
 #include "memory/Allocator.h"
+#include "util/Hex.h"
 #include "wire/Error.h"
 #include "wire/Message.h"
 
+#include <assert.h>
 #include <stdio.h>
 #include <event2/event.h>
 
@@ -32,6 +35,8 @@ static uint8_t* publicKey = (uint8_t*)
     "\xc3\xcf\xd9\x7e\x32\x04\x12\xb4\xaf\x7e\x22\x92\xa5\xdf\x31\x71";
 
 static struct event_base* eventBase;
+
+static uint8_t* hello = (uint8_t*) "Hello World";
 
 int encryptRndNonceTest()
 {
@@ -69,27 +74,57 @@ static uint8_t sendMessage(struct Message* message, struct Interface* iface)
     return Error_NONE;
 }
 
-int hello()
+// This needs to be determinent.
+void randombytes(unsigned char* buffer,unsigned long long size)
 {
-    uint8_t buff[BUFFER_SIZE];
-    struct Allocator* allocator = BufferAllocator_new(buff, BUFFER_SIZE);
-    struct Writer* logwriter = FileWriter_new(stdout, allocator);
-    struct Log logger = { .writer = logwriter };
-    struct CryptoAuth* ca = CryptoAuth_new(NULL, allocator, NULL, eventBase, &logger);
+    memset(buffer, 0xFF, size);
+}
 
-    struct Message* out = NULL;
-    struct Interface iface = {
-        .sendMessage = sendMessage,
-        .senderContext = &out
-    };
+struct Wrapper* setUp(uint8_t* myPrivateKey,
+                      uint8_t* herPublicKey,
+                      uint8_t* authPassword,
+                      struct Message** resultMessage)
+{
+    struct Allocator* allocator = MallocAllocator_new(8192*2);
+    struct Log* logger = allocator->malloc(sizeof(struct Log), allocator);
+    logger->writer = FileWriter_new(stdout, allocator);
+    struct CryptoAuth* ca = CryptoAuth_new(NULL, allocator, myPrivateKey, eventBase, logger);
 
-    struct Wrapper wrapper = {
-        .context = ca,
-        .wrappedInterface = &iface
-    };
-    memcpy(wrapper.herPerminentPubKey, publicKey, 32);
+    struct Interface* iface =
+        allocator->clone(sizeof(struct Interface), allocator, &(struct Interface) {
+            .sendMessage = sendMessage,
+            .senderContext = resultMessage
+        });
 
-    uint8_t* hello = (uint8_t*) "Hello World";
+    struct Wrapper* wrapper =
+        allocator->clone(sizeof(struct Wrapper), allocator, &(struct Wrapper) {
+            .context = ca,
+            .wrappedInterface = iface
+        });
+
+    if (authPassword) {
+        struct Interface temp = {
+            .senderContext = wrapper,
+            .allocator = allocator
+        };
+        String str = { .bytes = (char*) authPassword, .len = strlen((char*)authPassword) };
+        CryptoAuth_setAuth(&str, 1, &temp);
+    }
+
+    if (herPublicKey) {
+        memcpy(wrapper->herPerminentPubKey, herPublicKey, 32);
+    }
+
+    return wrapper;
+}
+
+void testHello(uint8_t* password, uint8_t* expectedOutput)
+{
+    assert(strlen((char*)expectedOutput) == 264);
+    struct Message* outMessage;
+    struct Wrapper* wrapper =
+        setUp(NULL, (uint8_t*) "wxyzabcdefghijklmnopqrstuv987654", password, &outMessage);
+
     uint8_t msgBuff[Headers_CryptoAuth_SIZE + 12];
     struct Message msg = {
         .length = 12,
@@ -97,23 +132,62 @@ int hello()
         .bytes = msgBuff + Headers_CryptoAuth_SIZE
     };
     memcpy(msg.bytes, hello, 12);
-    Exports_encryptHandshake(&msg, &wrapper);
+    Exports_encryptHandshake(&msg, wrapper);
 
-    // Check the nonce
-    assert(!memcmp(msg.bytes, "\0\0\0\0", 4));
+    uint8_t actual[265];
+    assert(Hex_encode(actual, 265, outMessage->bytes, outMessage->length) > 0);
+    //printf("%s", actual);
+    assert(!memcmp(actual, expectedOutput, 264));
+}
 
-    ca = CryptoAuth_new(NULL, allocator, privateKey, eventBase, &logger);
-    struct Message* finalOut = NULL;
-    struct Wrapper wrapper2 = {
-        .context = ca,
-        .externalInterface = {
-            .receiveMessage = receiveMessage,
-            .receiverContext = &finalOut
-        },
-        .wrappedInterface = &iface
+void helloNoAuth()
+{
+    uint8_t* expected = (uint8_t*)
+        "0000000000ffffffffffffff7fffffffffffffffffffffffffffffffffffffff"
+        "ffffffffffffffff847c0d2c375234f365e660955187a3735a0f7613d1609d3a"
+        "6a4d8c53aeaa5a22e6f55c4f45d6906e90ef53d53593d71a4f1af6484ceec3d2"
+        "691858481b2fe05d51aaba9a74925c4595fc57ab3287d1fb325a9d0aa238476b"
+        "f9a5c117";
+
+    testHello(NULL, expected);
+}
+
+void helloWithAuth()
+{
+    uint8_t* expected = (uint8_t*)
+        "0000000001641c99f7719f570000beb1ffffffffffffffffffffffffffffffff"
+        "ffffffffffffffff847c0d2c375234f365e660955187a3735a0f7613d1609d3a"
+        "6a4d8c53aeaa5a2289427cd94d2710830662b77ef3b00cd6aab129686fce50e9"
+        "823d7db9ff0b37c46a7dcfbb40a43ba7b42fb09dfed7d06fed814ddf977e3d9a"
+        "2cc44ab6";
+
+    testHello((uint8_t*)"password", expected);
+}
+
+int receiveHelloWithNoAuth()
+{
+    uint8_t* messageHex = (uint8_t*)
+        "0000000000ffffffffffffff7fffffffffffffffffffffffffffffffffffffff"
+        "ffffffffffffffff847c0d2c375234f365e660955187a3735a0f7613d1609d3a"
+        "6a4d8c53aeaa5a22ea9cf275eee0185edf7f211192f12e8e642a325ed76925fe"
+        "3c76d313b767a10aca584ca0b979dee990a737da7d68366fa3846d43d541de91"
+        "29ea3e12";
+
+    uint8_t message[132];
+    assert(Hex_decode(message, 132, messageHex, strlen((char*)messageHex)) > 0);
+    struct Message incoming = {
+        .length = 132,
+        .padding = 0,
+        .bytes = message
     };
 
-    Exports_receiveMessage(out, &(struct Interface) { .receiverContext = &wrapper2 } );
+    struct Wrapper* wrapper = setUp(privateKey, NULL, NULL, NULL);
+
+    struct Message* finalOut = NULL;
+    wrapper->externalInterface.receiveMessage = receiveMessage;
+    wrapper->externalInterface.receiverContext = &finalOut;
+
+    Exports_receiveMessage(&incoming, &(struct Interface) { .receiverContext = wrapper } );
 
     assert(finalOut);
     assert(finalOut->length == 12);
@@ -184,5 +258,8 @@ int repeatHello()
 int main()
 {
     eventBase = event_base_new();
-    return encryptRndNonceTest() | createNew() | hello() | repeatHello();
+    helloNoAuth();
+    helloWithAuth();
+    receiveHelloWithNoAuth();
+    return encryptRndNonceTest() | createNew() | repeatHello();
 }
