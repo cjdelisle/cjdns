e04c3cabca1e5a5b8aa53af96eaa37ea6c87528e
Caleb James DeLisle <calebdelisle@lavabit.com>
2012-04-17 05:38:51 -0400

    Utility to dump the table and ping all nodes in it.

---
+++ contrib/python/pingAll.py
@@ -0,0 +1,44 @@
+# You may redistribute this program and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys;
+from cjdns import cjdns_connect;
+
+if (len(sys.argv) < 4):
+    print("Usage " + sys.argv[0] + " <ip addr> <port> <password>");
+    exit(0);
+
+cjdns = cjdns_connect(sys.argv[1], int(sys.argv[2]), sys.argv[3]);
+addresses = {};
+
+i = 0;
+while True:
+    table = cjdns.NodeStore_dumpTable(i);
+    routes = table['routingTable'];
+    for entry in routes:
+        if (entry['link'] != 0):
+            addresses[entry['ip']] = entry['path'];
+        #print entry['ip'] + '@' + entry['path'] + ' - ' + str(entry['link']);
+    if (not 'more' in table):
+        break;
+    i += 1;
+
+i = 0;
+responses = 0;
+for addr in addresses:
+    result = cjdns.RouterModule_pingNode(addr, 2000);
+    if ('result' in result and result['result'] == 'pong'):
+        responses += 1;
+    print(addr + ' - ' + str(result));
+    i += 1;
+
+print(str(i) + ' total ' + str(responses) + ' respond.');
