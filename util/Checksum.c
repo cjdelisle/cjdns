929f4785dcd62b2dc4201ceb40e7bdb0d49462e5
Caleb James DeLisle <calebdelisle@lavabit.com>
2012-05-08 06:20:47 -0400

    Try a different way, hopefully suppress strict-aliasing error on mips.

--- util/Checksum.c
+++ util/Checksum.c
@@ -22,16 +22,16 @@ static uint32_t checksumStep(const uint8_t* buffer,
     uint32_t i;
 
     // Checksum pairs.
-    for (i = 0; i < (length & ~1u); i += 2) {
-        state += (uint16_t) Endian_bigEndianToHost16( *((uint16_t*) &buffer[i]) );
+    for (i = 0; i * 2 < length; i++) {
+        state += (uint16_t) Endian_bigEndianToHost16( ((uint16_t*) buffer)[i] );
         if (state > 0xFFFF) {
             state -= 0xFFFF;
         }
     }
 
     // Do the odd byte if there is one.
-    if (i < length) {
-        state += buffer[i] << 8;
+    if (i % 2) {
+        state += buffer[i * 2 - 1] << 8;
         if (state > 0xFFFF) {
             state -= 0xFFFF;
         }
