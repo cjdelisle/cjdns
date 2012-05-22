d90380b7ca6dd0379108375b574320d2d874dbdd
Stefan Bühler <stbuehler@web.de>
2012-05-23 00:16:56 +0200

    Improve number compression unit test - test labels from 0-0xffff too, test all bit widths

--- switch/test/NumberCompress_test.c
+++ switch/test/NumberCompress_test.c
@@ -18,67 +18,72 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-static const uint64_t MAGIC = 0xa984f5b; // last bit should be set.
+static void numberCompressions_generic(
+    uint32_t nInterfaces,
+    uint32_t (*bitsUsedForLabel)(const uint64_t label),
+    uint32_t (*bitsUsedForNumber)(const uint32_t number),
+    uint64_t (*getCompressed)(const uint32_t number, const uint32_t bitsUsed),
+    uint32_t (*getDecompressed)(const uint64_t label, const uint32_t bitsUsed)) {
 
-static void numberCompressions_4() {
-    for (uint32_t i = 0; i < NumberCompress_4_INTERFACES; ++i) {
-        uint32_t bits = NumberCompress_4_bitsUsedForNumber(i);
-        uint64_t label = NumberCompress_4_getCompressed(i, bits);
-        if (1 == i) {
-            Assert_always(1 == label);
-            Assert_always(4 == bits);
+    uint8_t bitWidths[64] = { 0 };
+
+    for (uint32_t i = 0; i < nInterfaces; ++i) {
+        bitWidths[bitsUsedForNumber(i)] = 1;
+    }
+
+    for (uint32_t bits = 0; bits < 64; ++bits) {
+        if (!bitWidths[bits]) continue;
+
+        for (uint32_t i = 0; i < nInterfaces; ++i) {
+            /* only check for greater-or-equal bit widths */
+            if (bits < bitsUsedForNumber(i)) continue;
+
+            uint64_t label = getCompressed(i, bits);
+
+            if (1 == i) {
+                Assert_always(1 == label);
+                continue;
+            }
+
+            Assert_always(bits == bitsUsedForLabel(label));
+            Assert_always(i == getDecompressed(label, bits));
         }
-        Assert_always(bits == NumberCompress_4_bitsUsedForLabel(label));
-        Assert_always(i == NumberCompress_4_getDecompressed(label, bits));
+    }
 
-        // put some other stuff before the discrimator
-        label |= MAGIC << bits;
-        Assert_always(bits == NumberCompress_4_bitsUsedForLabel(label));
-        Assert_always(i == NumberCompress_4_getDecompressed(label, bits));
+    for (uint64_t label = 0; label < 0x10000u; ++label) {
+        uint32_t bits = bitsUsedForLabel(label);
+        Assert_always(1 == bitWidths[bits]);
+        if (1 == (label & 0xf)) {
+            Assert_always(4 == bits);
+            Assert_always(1 == getDecompressed(label, 4));
+        } else {
+            uint32_t i = getDecompressed(label, bits);
+            Assert_always(i < nInterfaces);
+        }
     }
 }
-
-static void numberCompressions_8() {
-    for (uint32_t i = 0; i < NumberCompress_8_INTERFACES; ++i) {
-        uint32_t bits = NumberCompress_8_bitsUsedForNumber(i);
-        uint64_t label = NumberCompress_8_getCompressed(i, bits);
-        if (1 == i) {
-            Assert_always(1 == label);
-            Assert_always(4 == bits);
-        }
-        Assert_always(bits == NumberCompress_8_bitsUsedForLabel(label));
-        Assert_always(i == NumberCompress_8_getDecompressed(label, bits));
-
-        // put some other stuff before the discrimator
-        label |= MAGIC << bits;
-        Assert_always(bits == NumberCompress_8_bitsUsedForLabel(label));
-        Assert_always(i == NumberCompress_8_getDecompressed(label, bits));
-    }
-}
-
-static void numberCompressions_dyn() {
-    for (uint32_t i = 0; i < NumberCompress_dyn_INTERFACES; ++i) {
-        uint32_t bits = NumberCompress_dyn_bitsUsedForNumber(i);
-        uint64_t label = NumberCompress_dyn_getCompressed(i, bits);
-        if (1 == i) {
-            Assert_always(1 == label);
-            Assert_always(4 == bits);
-        }
-        Assert_always(bits == NumberCompress_dyn_bitsUsedForLabel(label));
-        Assert_always(i == NumberCompress_dyn_getDecompressed(label, bits));
-
-        // put some other stuff before the discrimator
-        label |= MAGIC << bits;
-        Assert_always(bits == NumberCompress_dyn_bitsUsedForLabel(label));
-        Assert_always(i == NumberCompress_dyn_getDecompressed(label, bits));
-    }
-}
-
 
 int main()
 {
-    numberCompressions_4();
-    numberCompressions_8();
-    numberCompressions_dyn();
+    numberCompressions_generic(
+        NumberCompress_4_INTERFACES,
+        NumberCompress_4_bitsUsedForLabel,
+        NumberCompress_4_bitsUsedForNumber,
+        NumberCompress_4_getCompressed,
+        NumberCompress_4_getDecompressed);
+
+    numberCompressions_generic(
+        NumberCompress_8_INTERFACES,
+        NumberCompress_8_bitsUsedForLabel,
+        NumberCompress_8_bitsUsedForNumber,
+        NumberCompress_8_getCompressed,
+        NumberCompress_8_getDecompressed);
+
+    numberCompressions_generic(
+        NumberCompress_dyn_INTERFACES,
+        NumberCompress_dyn_bitsUsedForLabel,
+        NumberCompress_dyn_bitsUsedForNumber,
+        NumberCompress_dyn_getCompressed,
+        NumberCompress_dyn_getDecompressed);
     return 0;
 }
