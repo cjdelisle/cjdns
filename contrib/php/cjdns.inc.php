b27cf67d37ee512cca3b3b2391303c048953d8a6
Finn Herzfeld <thefinn93@thefinn93.com>
2012-07-18 13:06:26 -0700

    Added license

--- contrib/php/cjdns.inc.php
+++ contrib/php/cjdns.inc.php
@@ -1,10 +1,20 @@
 <?php
 /*
- CJDNS admin interface for PHP. See index.php for sample usage
- https://github.com/thefinn93/cjdns/ contrib/php/
+ * You may redistribute this program and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
 
- 20120718 ircerr@EFNet
- Partially Moded for fscockopen vs socket_create for compatability
+ CJDNS admin interface for PHP. See index.php for sample usage
+ https://github.com/thefinn93/cjdns/contrib/php/
 
 */
 
