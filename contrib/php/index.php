27000c305cb32d9b8fe4580a0a85eb0590788212
Finn Herzfeld <thefinn93@thefinn93.com>
2012-07-18 12:45:24 -0700

    ircerr partially Moded for fscockopen vs socket_create for compatability, added full <?php tags for derp and redid the formatting to be all mainstream. Moved includes to .inc.php files. Added a secrets.php file the contains the connection details. Fixed the functionlist() function, which didn't work before

--- contrib/php/index.php
+++ contrib/php/index.php
@@ -1,10 +1,16 @@
 <pre>
-<?
-require_once("cjdns.php");
+<?php
+require_once("cjdns.inc.php");
+
 $admin = new cjdns;
-$admin->connect("supersecretpassword");
+
+$admin->connect();
+
 print_r($admin->call("RouterModule_pingNode",array("path"=>"fc72:6c3b:8c74:68a7:d8c3:b4e0:6cbd:9588")));
+
 $admin->functionlist();
+
 $admin->disconnect();
+
 ?>
 </pre>
