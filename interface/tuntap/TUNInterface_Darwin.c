10d99b6a18c3e79bb6e6e3ce07ec24c2c5f03720
Caleb James DeLisle <calebdelisle@lavabit.com>
2013-05-26 20:29:35 -0400

    debugging apple

--- interface/tuntap/TUNInterface_Darwin.c
+++ interface/tuntap/TUNInterface_Darwin.c
@@ -14,7 +14,7 @@
  */
 #include "exception/Except.h"
 #include "interface/Interface.h"
-#include "interface/tunatp/TUNInterface.h"
+#include "interface/tuntap/TUNInterface.h"
 #include "util/AddrTools.h"
 
 #include <errno.h>
