2bc4d596217f8f95bf54c207b683e8e86e06c176
Caleb James DeLisle <calebdelisle@lavabit.com>
2013-05-26 20:58:05 -0400

    Fixed broken BSDMessageTypeWrapper

--- interface/tuntap/BSDMessageTypeWrapper.c
+++ interface/tuntap/BSDMessageTypeWrapper.c
@@ -12,73 +12,88 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "interface/tuntap/BSDMessageTypeWrapper.h"
 #include "interface/Interface.h"
 #include "interface/InterfaceWrapper.h"
+#include "util/platform/Sockaddr.h"
 #include "memory/Allocator.h"
 #include "util/Assert.h"
 #include "util/Identity.h"
 #include "wire/Ethernet.h"
 #include "wire/Message.h"
-
+#include "wire/Error.h"
 
 /**
- * OSX and BSD is expect you to send the platform dependent
- * address family type rather than the ethertype.
+ * OSX and BSD is expect you to send the platform dependent address family type
+ * rather than the ethertype, this InterfaceWrapper converts AF_INET and AF_INET6 to the
+ * corrisponding ethertypes and back.
  */
-static const int AF_INET6_BE = Endian_hostToBigEndian16(Sockaddr_AF_INET6);
-static const int AF_INET_BE = Endian_hostToBigEndian16(Sockaddr_AF_INET);
+
+struct BSDMessageTypeWrapper_pvt
+{
+    struct Interface generic;
+    struct Interface* const wrapped;
+    const uint16_t afInet_be;
+    const uint16_t afInet6_be;
+    struct Log* const logger;
+    Identity
+};
 
 static uint8_t receiveMessage(struct Message* msg, struct Interface* iface)
 {
     struct BSDMessageTypeWrapper_pvt* ctx =
         Identity_cast((struct BSDMessageTypeWrapper_pvt*)iface->receiverContext);
 
-    if (message->length < 4) {
+    if (msg->length < 4) {
         return Error_NONE;
     }
 
     uint16_t afType_be = ((uint16_t*) msg->bytes)[1];
     uint16_t ethertype = 0;
-    switch (afType_be) {
-        case AF_INET6_BE: ethertype = Ethernet_TYPE_IP6; break;
-        case AF_INET_BE: ethertype = Ethernet_TYPE_IP4; break;
-        default: {
-            Log_debug(logger, "Message of unhandled aftype [%d]",
-                      Endian_bigEndianToHost16(afType_be));
-            return Error_NONE;
-        }
+    if (afType_be == ctx->afInet_be) {
+        ethertype = Ethernet_TYPE_IP4;
+    } else if (afType_be == ctx->afInet6_be) {
+        ethertype = Ethernet_TYPE_IP6;
+    } else {
+        Log_debug(ctx->logger, "Message of unhandled aftype [%d]",
+                  Endian_bigEndianToHost16(afType_be));
+        return Error_NONE;
     }
     ((uint16_t*) msg->bytes)[0] = 0;
     ((uint16_t*) msg->bytes)[0] = ethertype;
 
-    Interface_receiveMessage(&ctx->generic, msg);
+    return Interface_receiveMessage(&ctx->generic, msg);
 }
 
 static uint8_t sendMessage(struct Message* msg, struct Interface* iface)
 {
     struct BSDMessageTypeWrapper_pvt* ctx = Identity_cast((struct BSDMessageTypeWrapper_pvt*)iface);
 
-    Assert_true(message->length >= 4);
+    Assert_true(msg->length >= 4);
 
     uint16_t ethertype = ((uint16_t*) msg->bytes)[1];
     uint16_t afType_be = 0;
-    switch (ethertype) {
-        case Ethernet_TYPE_IP6: afType_be = AF_INET6_BE; break;
-        case Ethernet_TYPE_IP4: afType_be = AF_INET_BE; break;
-        default Assert_always(!"Unsupported ethertype");
+    if (ethertype == Ethernet_TYPE_IP6) {
+        afType_be = ctx->afInet6_be;
+    } else if (ethertype == Ethernet_TYPE_IP4) {
+        afType_be = ctx->afInet_be;
+    } else {
+        Assert_always(!"Unsupported ethertype");
     }
     ((uint16_t*) msg->bytes)[0] = 0;
     ((uint16_t*) msg->bytes)[1] = afType_be;
 
-    Interface_sendMessage(ctx->wrapped, msg);
+    return Interface_sendMessage(ctx->wrapped, msg);
 }
 
-struct Interface* BSDMessageTypeWrapper_new(struct Interface* wrapped, struct Allocator* alloc)
+struct Interface* BSDMessageTypeWrapper_new(struct Interface* wrapped, struct Log* logger)
 {
     struct BSDMessageTypeWrapper_pvt* context =
-        Allocator_clone(alloc, (&(struct BSDMessageTypeWrapper_pvt) {
-            .wrapped = wrappedIface,
-            .alloc = alloc
+        Allocator_clone(wrapped->allocator, (&(struct BSDMessageTypeWrapper_pvt) {
+            .wrapped = wrapped,
+            .afInet6_be = Endian_hostToBigEndian16(Sockaddr_AF_INET6),
+            .afInet_be = Endian_hostToBigEndian16(Sockaddr_AF_INET),
+            .logger = logger
         }));
     Identity_set(context);
 
