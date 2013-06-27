cd78a6f9de969a8c6087d04fd46290e3e413846e
norlin <myxaxaxa@gmail.com>
2013-06-27 20:35:46 +0400

    Add some basic stuff to client-backend interaction

--- contrib/nodejs/admin/www/js/script.js
+++ contrib/nodejs/admin/www/js/script.js
@@ -1,3 +1,12 @@
 (function (window, $) {
+    function init () {
+        //Check if backend working
+        $.post('/api', {lorem: 'ipsum'}, function (res) {
+            if (!res.hello || res.hello !== 'world!') {
+                alert('Check your node.js backend!');
+            }
+        });
+    }
 
+    $(init);
 } (window, jQuery));
