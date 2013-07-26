6f8c25d8b87d50dc0ce725fce6393e3853721677
norlin <myxaxaxa@gmail.com>
2013-07-26 13:26:17 +0400

    Nodejs Web Admin: switch config parsing to eval
    Because config file is not really JSON, it's JS-file with comments, etc.

--- contrib/nodejs/admin/cjdns.js
+++ contrib/nodejs/admin/cjdns.js
@@ -36,94 +36,11 @@ var CJDNS = function (config) {
 };
 
 CJDNS.prototype.parseConfig = function() {
-    function removeComments(str) {
-        /*
-        http://james.padolsey.com/javascript/removing-comments-in-javascript/
-         */
-        str = ('__' + str + '__').split('');
-        var mode = {
-            singleQuote: false,
-            doubleQuote: false,
-            regex: false,
-            blockComment: false,
-            lineComment: false,
-            condComp: false
-        };
-        for (var i = 0, l = str.length; i < l; i++) {
+    var config;
 
-            if (mode.regex) {
-                if (str[i] === '/' && str[i-1] !== '\\') {
-                    mode.regex = false;
-                }
-                continue;
-            }
+    eval('config = ' + this.config);
 
-            if (mode.singleQuote) {
-                if (str[i] === "'" && str[i-1] !== '\\') {
-                    mode.singleQuote = false;
-                }
-                continue;
-            }
-
-            if (mode.doubleQuote) {
-                if (str[i] === '"' && str[i-1] !== '\\') {
-                    mode.doubleQuote = false;
-                }
-                continue;
-            }
-
-            if (mode.blockComment) {
-                if (str[i] === '*' && str[i+1] === '/') {
-                    str[i+1] = '';
-                    mode.blockComment = false;
-                }
-                str[i] = '';
-                continue;
-            }
-
-            if (mode.lineComment) {
-                if (str[i+1] === '\n' || str[i+1] === '\r') {
-                    mode.lineComment = false;
-                }
-                str[i] = '';
-                continue;
-            }
-
-            if (mode.condComp) {
-                if (str[i-2] === '@' && str[i-1] === '*' && str[i] === '/') {
-                    mode.condComp = false;
-                }
-                continue;
-            }
-
-            mode.doubleQuote = str[i] === '"';
-            mode.singleQuote = str[i] === "'";
-
-            if (str[i] === '/') {
-
-                if (str[i+1] === '*' && str[i+2] === '@') {
-                    mode.condComp = true;
-                    continue;
-                }
-                if (str[i+1] === '*') {
-                    str[i] = '';
-                    mode.blockComment = true;
-                    continue;
-                }
-                if (str[i+1] === '/') {
-                    str[i] = '';
-                    mode.lineComment = true;
-                    continue;
-                }
-                mode.regex = true;
-
-            }
-
-        }
-        return str.join('').slice(2, -2);
-    }
-
-    this.config = JSON.parse(removeComments(this.config));
+    this.config = config;
 };
 
 CJDNS.prototype.checkConfig = function (config) {
