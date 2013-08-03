d6e37b879e9cfc600d5966cbab726653e08d8f68
Philip Horger <campadrenalin@gmail.com>
2013-08-02 23:20:52 -0700

    #275 Working code

--- contrib/lua/test.lua
+++ contrib/lua/test.lua
@@ -1,12 +1,26 @@
 cjdns = require "cjdns"
 
+-- CONFIG
+
 confpath = "/etc/cjdroute.conf"
+
+-- SETUP
 
 print("Using config file:", confpath)
 conf = cjdns.ConfigFile.new(confpath)
 ai   = conf:makeInterface()
 
+-- TESTS
+
 print("Can I ping the admin server?")
 print("ping", ai:ping())
+
+print("Can I get a cookie?")
+print("cookie", ai:getCookie())
+
+print("Can I ping via auth?")
+auth_ping = ai:auth({q = "ping"})
+print("ping", (auth_ping and auth_ping.q == "pong"))
+
 print("Exporting compressed conf to test.conf...")
 print(conf:save("test.conf"), "done")
