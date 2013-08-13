d33156ae59825f8b7be6045be0378ba9660430b1
Matt Hartzler <matt.hartzler@gmail.com>
2013-08-13 09:10:27 -0500

    paginate the response

--- interface/InterfaceController_admin.c
+++ interface/InterfaceController_admin.c
@@ -29,28 +29,40 @@ struct Context
     struct Admin* admin;
 };
 
+// typical peer record is around 140 benc chars, so can't have very many in 1023
+#define ENTRIES_PER_PAGE 6
 static void adminPeerStats(Dict* args, void* vcontext, String* txid)
 {
     struct Context* context = vcontext;
     struct Allocator* alloc = Allocator_child(context->alloc);
     struct InterfaceController_peerStats* stats = NULL;
 
+    int64_t* page = Dict_getInt(args, String_CONST("page"));
+    int i = (page) ? *page * ENTRIES_PER_PAGE : 0;
+
     int count = context->ic->getPeerStats(context->ic, alloc, &stats);
 
     List* list = NULL;
-    for (int i = 0; i < count; i++) {
+    for (int counter=0; i < count && counter++ < ENTRIES_PER_PAGE; i++) {
         Dict* d = Dict_new(alloc);
         Dict_putInt(d, String_CONST("bytesIn"), stats[i].bytesIn, alloc);
         Dict_putInt(d, String_CONST("bytesOut"), stats[i].bytesOut, alloc);
         Dict_putString(d, String_CONST("pubkey"), Key_stringify(stats[i].pubKey, alloc), alloc);
         Dict_putInt(d, String_CONST("state"), stats[i].state, alloc);
+        Dict_putInt(d, String_CONST("last"), stats[i].timeOfLastMessage, alloc);
         list = List_addDict(list, d, alloc);
     }
 
     Dict response = Dict_CONST(
-        String_CONST("count"), Int_OBJ(count), Dict_CONST(
-        String_CONST("peers"), List_OBJ(list), NULL
+        String_CONST("peers"), List_OBJ(list), Dict_CONST(
+        String_CONST("total"), Int_OBJ(count), NULL
     ));
+
+    if (i < count) {
+      response = Dict_CONST(
+        String_CONST("more"), Int_OBJ(1), response
+      );
+    }
 
     Admin_sendMessage(&response, txid, context->admin);
 
@@ -69,6 +81,6 @@ void InterfaceController_admin_register(struct InterfaceController* ic,
 
     Admin_registerFunction("InterfaceController_peerStats", adminPeerStats, ctx, true,
         ((struct Admin_FunctionArg[]) {
-            { .name = "pubkey", .required = 0, .type = "String" }
+            { .name = "page", .required = 0, .type = "Int" }
         }), admin);
 }
