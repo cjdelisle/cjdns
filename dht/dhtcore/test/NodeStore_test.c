5ba1f03f88753f28b678fd75aae251d834b9f3f2
hartzler <hartzler@cryptopanic.org>
2013-09-12 12:06:48 -0500

    add simple tests for addNode / getNodeByAddr

--- dht/dhtcore/test/NodeStore_test.c
+++ dht/dhtcore/test/NodeStore_test.c
@@ -14,28 +14,76 @@
  */
 #include "memory/MallocAllocator.h"
 #include "crypto/random/Random.h"
-#include "util/Assert.h"
 #include "dht/Address.h"
 #include "dht/dhtcore/Node.h"
+#include "dht/dhtcore/NodeList.h"
 #include "dht/dhtcore/NodeStore.h"
 #include "dht/dhtcore/NodeStore_pvt.h"
+#include "io/FileWriter.h"
+#include "util/Assert.h"
+#include "util/Endian.h"
+#include "util/log/WriterLog.h"
+#include "util/version/Version.h"
 
 #include <stddef.h>
 #include <stdio.h>
 
-//struct NodeStore* store = NULL;
+struct Allocator* alloc = NULL;
+struct Random* rand = NULL;
+struct Log* logger = NULL;
 
-void setUp()
+struct NodeStore* setUp(struct Address* myAddress, uint32_t capacity)
 {
+    return NodeStore_new(myAddress, capacity, alloc, logger, rand);
+}
 
+struct Address* randomAddress()
+{
+  // horrible hack...
+  return Allocator_clone(alloc, (&(struct Address) {
+    .ip6.ints = {
+      .three = Endian_bigEndianToHost32(0xfc000000 + Random_uint16(rand)),
+      .four = Random_uint32(rand),
+      .two = Random_uint32(rand),
+      .one = Random_uint32(rand)
+    },
+    .path = 0x13
+  }));
+}
+
+void test_addNode()
+{
+    struct Address* myAddr = randomAddress();
+    struct NodeStore* store = setUp(myAddr, 8);
+    // adding ourselves should be null
+    Assert_always(NodeStore_addNode(store, myAddr, 0, Version_CURRENT_PROTOCOL) == NULL);
+    // adding somebody else should not
+    Assert_always(NodeStore_addNode(store, randomAddress(), 0, Version_CURRENT_PROTOCOL) != NULL);
+}
+
+void test_getNodesByAddr()
+{
+    struct Address* myAddr = randomAddress();
+    struct NodeStore* store = setUp(myAddr, 8);
+    struct Address* otherAddr = randomAddress();
+
+    // make sure we can add an addr and get it back
+    NodeStore_addNode(store, otherAddr, 0, Version_CURRENT_PROTOCOL);
+    struct NodeList* list = NodeStore_getNodesByAddr(otherAddr, 1, alloc, store);
+    Assert_always(list != NULL);
+    Assert_always(list->size == 1);
+    Assert_always(Address_isSame(&list->nodes[0]->address, otherAddr));
 }
 
 int main()
 {
-    struct Allocator* alloc = MallocAllocator_new(1<<20);
-    //struct Random* rand = Random_new(alloc, NULL, NULL);
+    alloc = MallocAllocator_new(1<<20);
+    struct Writer* writer = FileWriter_new(stdout, alloc);
+    logger = WriterLog_new(writer, alloc);
+    rand = Random_new(alloc, NULL, NULL);
 
-    setUp();
+    test_addNode();
+    test_getNodesByAddr();
 
     Allocator_free(alloc);
 }
