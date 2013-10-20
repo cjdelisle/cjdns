1d06e2ad0eb7f229db1516aa58ffd3079682d138
Robert Collins <robertc@robertcollins.net>
2013-10-20 19:56:40 +1300

    Backout broken change to ip6tables.
    /usr/bin/env sh -e doesn't work.
    
    Fixes issue 370.

--- contrib/bash/ip6tables.sh
+++ contrib/bash/ip6tables.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh -e
+#!/bin/sh -e
 #
 # You may redistribute this program and/or modify it under the terms of
 # the GNU General Public License as published by the Free Software Foundation,
