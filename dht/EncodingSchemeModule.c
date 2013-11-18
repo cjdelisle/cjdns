3fbf41449af56b52b4ed18218c402e1222026ea1
Caleb James DeLisle <cjd@hyperboria.ca>
2013-11-18 20:49:15 +0100

    Roll back some undesirable changes to EncodingSchemeModule and fixed a bug

--- dht/EncodingSchemeModule.c
+++ dht/EncodingSchemeModule.c
@@ -25,33 +25,6 @@
 #include "util/version/Version.h"
 #include "dht/dhtcore/NodeStore.h"
 
-struct EncodingSchemeModule_pvt;
-
-struct SchemeAndIndex
-{
-    /** The peer's encoding scheme. */
-    String* schemeDefinition;
-
-    /** The peer's encoding scheme form index. */
-    int encIdx;
-
-    /** Our interface index. */
-    uint32_t ifIndex;
-
-    /** The module. */
-    struct EncodingSchemeModule_pvt* ctx;
-
-    /** Allocator for this SchemeAndIndex. */
-    struct Allocator* alloc;
-
-    Identity
-};
-
-#define Map_VALUE_TYPE struct SchemeAndIndex*
-#define Map_KEY_TYPE uint32_t
-#define Map_NAME OfPeerSchemes
-#include "util/Map.h"
-
 /**
  * The encoding scheme module tags each message with our encoding scheme
  * representation as well as the number of the smallest encoding form which
@@ -75,21 +48,8 @@ struct EncodingSchemeModule_pvt
 
     struct Log* logger;
 
-    struct Map_OfPeerSchemes peerSchemeMap;
-
-    struct Allocator* alloc;
-
     Identity
 };
-
-static int peerSchemeOnFree(struct Allocator_OnFreeJob* job)
-{
-    struct SchemeAndIndex* sai = Identity_cast((struct SchemeAndIndex*) job->userData);
-    int mapIndex = Map_OfPeerSchemes_indexForKey(&sai->ifIndex, &sai->ctx->peerSchemeMap);
-    Assert_true(mapIndex > -1);
-    Map_OfPeerSchemes_remove(mapIndex, &sai->ctx->peerSchemeMap);
-    return 0;
-}
 
 static int handleIncoming(struct DHTMessage* message, void* vcontext)
 {
@@ -114,38 +74,12 @@ static int handleIncoming(struct DHTMessage* message, void* vcontext)
     }
 
     int64_t* encIdx = Dict_getInt(message->asDict, CJDHTConstants_ENC_INDEX);
-    if (!encIdx || *encIdx <= 0 || *encIdx >= scheme->count) {
+    if (!encIdx || *encIdx < 0 || *encIdx >= scheme->count) {
         Log_debug(ctx->logger, "Invalid encoding index");
         return -1;
     }
 
     NodeStore_discoverNode(ctx->ns, message->address, 2, *version, scheme, *encIdx);
-
-    int bits = NumberCompress_bitsUsedForLabel(message->address->path);
-    if (Bits_log2x64(message->address->path) == bits) {
-        // This node is a peer.
-        uint32_t ifIndex = NumberCompress_getDecompressed(message->address->path, bits);
-        int mapIndex = Map_OfPeerSchemes_indexForKey(&ifIndex, &ctx->peerSchemeMap);
-        if (mapIndex > -1
-          && String_equals(ctx->peerSchemeMap.values[mapIndex]->schemeDefinition, schemeDefinition)
-          && ctx->peerSchemeMap.values[mapIndex]->encIdx == *encIdx)
-        {
-            return 0;
-        }
-        if (mapIndex > -1) {
-            Allocator_free(ctx->peerSchemeMap.values[mapIndex]->alloc);
-        }
-        struct Allocator* peerSchemeAlloc = Allocator_child(ctx->alloc);
-        struct SchemeAndIndex* sai =
-            Allocator_calloc(peerSchemeAlloc, sizeof(struct SchemeAndIndex), 1);
-        sai->schemeDefinition = String_clone(schemeDefinition, peerSchemeAlloc);
-        sai->encIdx = *encIdx;
-        sai->ifIndex = ifIndex;
-        sai->alloc = peerSchemeAlloc;
-        sai->ctx = ctx;
-        Identity_set(sai);
-        Allocator_onFree(peerSchemeAlloc, peerSchemeOnFree, sai);
-    }
 
     return 0;
 }
@@ -164,21 +98,6 @@ static int handleOutgoing(struct DHTMessage* dmesg, void* vcontext)
     // And tell the asker which interface the message came from
     int encIdx = EncodingScheme_getFormNum(ctx->scheme, dmesg->address->path);
     Dict_putInt(dmesg->asDict, CJDHTConstants_ENC_INDEX, encIdx, dmesg->allocator);
-
-    // Then tell them the scheme and form index for the nearest node along the path.
-    int totalBits = NumberCompress_bitsUsedForLabel(dmesg->address->path);
-    uint32_t ifIndex = NumberCompress_dyn_getDecompressed(dmesg->address->path, totalBits);
-    int mapIndex = Map_OfPeerSchemes_indexForKey(&ifIndex, &ctx->peerSchemeMap);
-    if (mapIndex > -1) {
-        Dict_putString(dmesg->asDict,
-                       CJDHTConstants_PEER_ENC_SCHEME,
-                       ctx->peerSchemeMap.values[mapIndex]->schemeDefinition,
-                       dmesg->allocator);
-        Dict_putInt(dmesg->asDict,
-                    CJDHTConstants_PEER_ENC_INDEX,
-                    ctx->peerSchemeMap.values[mapIndex]->encIdx,
-                    dmesg->allocator);
-    }
 
     return 0;
 }
@@ -201,11 +120,7 @@ void EncodingSchemeModule_register(struct DHTModuleRegistry* reg,
             .logger = logger,
             .scheme = scheme,
             .schemeDefinition = schemeDefinition,
-            .ns = ns,
-            .alloc = alloc,
-            .peerSchemeMap = {
-                .allocator = alloc
-            }
+            .ns = ns
         }));
     Identity_set(ctx);
     ctx->module.context = ctx;
