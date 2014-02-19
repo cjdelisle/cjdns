fade117c01d8f313e25aa8d69e18ea825b6f0b38
Lars Gierth <larsg@systemli.org>
2014-02-20 00:56:15 +0100

    Fix NaCl tests
    Compiling cjdns using the Android NDK's GCC toolchain failed
    because ld wouldn't find random().

--- node_build/dependencies/cnacl/tests/box8.c
+++ node_build/dependencies/cnacl/tests/box8.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "crypto_box.h"
 #include "randombytes.h"
 
--- node_build/dependencies/cnacl/tests/secretbox8.c
+++ node_build/dependencies/cnacl/tests/secretbox8.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "crypto_secretbox.h"
 #include "randombytes.h"
 
