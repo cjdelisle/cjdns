831c9756891657a41a0b94024d9bc69fc6048db8
Caleb James DeLisle <cjd@hyperboria.ca>
2014-05-05 09:39:02 +0200

    Compiler errors inside of preprocessor conditionals :|

--- util/ArchInfo.c
+++ util/ArchInfo.c
@@ -145,7 +145,7 @@ char* ArchInfo_describe(enum ArchInfo ai, struct Allocator* alloc)
     return out;
 }
 
-static enum ArchInfo detectBits()
+static enum ArchInfo_Bits detectBits()
 {
     switch (sizeof(long)) {
         case 4: return ArchInfo_Bits_32;
@@ -194,94 +194,109 @@ static enum ArchInfo_Endian assertEndian(enum ArchInfo_Endian end)
 
 enum ArchInfo ArchInfo_detect()
 {
+    enum ArchInfo arch;
+
     #if defined(__amd64__) || defined(__x86_64__) || defined(__AMD64__) || \
         defined(_M_X64) || defined(__amd64)
-
-        return ArchInfo_Arch_AMD64
-            | assertBits(ArchInfo_Bits_64)
-            | assertEndian(ArchInfo_Endian_LITTLE);
-
+        arch = ArchInfo_Arch_AMD64;
     #elif defined(__i386__) || defined(__x86__) || defined(__X86__) || \
           defined(_M_IX86) || defined(__i386)
-
-        uint32_t out = ArchInfo_I386
-            | assertBits(ArchInfo_Arch_32)
-            | assertEndian(ArchInfo_Endian_LITTLE);
-
-        #ifdef __SSE2__
-            out |= ArchInfo_I386_SSE2
-        #endif
-
+        arch = ArchInfo_Arch_I386;
     #elif defined(__ARM_EABI__) || defined(__arm__)
-
-        uint32_t out = ArchInfo_ARM
-            | detectBits()
-            | detectEndian();
-
-        #ifdef __ARM_EABI__
-            out |= ArchInfo_Arm_EABI;
-        #endif
-        #ifdef __ARM_NEON__
-            out |= ArchInfo_Arm_NEON;
-        #endif
-
-        return out;
-
+        arch = ArchInfo_Arch_ARM;
     #elif defined(__powerpc64__) || defined(__ppc64__) || defined(__PPC64__) || defined(_ARCH_PPC64)
-
-        return ArchInfo_PPC
-            | assertBits(ArchInfo_Arch_64)
-            | assertEndian(ArchInfo_Endian_BIG);
-
+        arch = ArchInfo_Arch_PPC | ArchInfo_Bits_64;
     #elif defined(__powerpc__) || defined(__ppc__) || defined(__PPC__) || defined(_ARCH_PPC)
-
-        return ArchInfo_PPC
-            | assertBits(ArchInfo_Arch_32)
-            | assertEndian(ArchInfo_Endian_BIG);
-
+        arch = ArchInfo_Arch_PPC | ArchInfo_Bits_32;
     #elif defined(__sparcv9__) || defined(__sparcv9)
-
-        return ArchInfo_SPARC
-            | ArchInfo_Sparc_v9
-            | assertBits(ArchInfo_Arch_64)
-            | assertEndian(ArchInfo_Endian_BIG);
-
+        arch = ArchInfo_Arch_SPARC | ArchInfo_Sparc_v9;
     #elif defined(__sparc_v8__)
-
-        return ArchInfo_SPARC
-            | ArchInfo_Sparc_v8
-            | assertBits(ArchInfo_Arch_32)
-            | assertEndian(ArchInfo_Endian_BIG);
-
+        arch = ArchInfo_Arch_SPARC | ArchInfo_Sparc_v8;
     #elif defined(__sparc__) || defined(__sparc)
-
-        uint32_t out = ArchInfo_SPARC | detectBits() | assertEndian(ArchInfo_Endian_BIG);
-        switch (ArchInfo_getBits(arch)) {
-            case ArchInfo_Arch_32: return out | ArchInfo_Sparc_v8;
-            case ArchInfo_Arch_64: return out | ArchInfo_Sparc_v9;
-            default return ArchInfo_UNKNOWN;
-        }
-
+        arch = ArchInfo_Arch_SPARC;
     #elif defined(__mips__) || defined(__mips) || defined(__MIPS__)
-
-        int out = ArchInfo_MIPS | detectEndian();
-
-        #if defined(_ABIO32)
-
-            return out | assertBits(ArchInfo_Arch_32) | ArchInfo_Mips_OABI;
-
-        #elif defined(_ABIN32)
-
-            return out | assertBits(ArchInfo_Arch_32) | ArchInfo_Mips_NABI;
-
-        #else
-
-            // If it's not oabi or nabi and it's not mips64 then it's UNKNOWN.
-            return out | assertBits(ArchInfo_Arch_64);
-
-        #endif
-
+        arch = ArchInfo_Arch_MIPS;
+    #else
+        arch = ArchInfo_UNKNOWN;
     #endif
 
-    return ArchInfo_UNKNOWN;
+    switch ((int)arch) {
+        case ArchInfo_Arch_AMD64:
+            return arch | assertBits(ArchInfo_Bits_64) | assertEndian(ArchInfo_Endian_LITTLE);
+        case ArchInfo_Arch_I386: {
+            #define HAS_SSE2 0
+            #ifdef __SSE2__
+                #undef HAS_SSE2
+                #define HAS_SSE2 1
+            #endif
+            if (HAS_SSE2) {
+                arch |= ArchInfo_I386_SSE2;
+            }
+            return arch | assertBits(ArchInfo_Bits_32) | assertEndian(ArchInfo_Endian_LITTLE);
+        }
+        case ArchInfo_Arch_ARM: {
+            #define HAS_EABI 0
+            #ifdef __ARM_EABI__
+                #undef HAS_EABI
+                #define HAS_EABI 1
+            #endif
+            if (HAS_EABI) {
+                arch |= ArchInfo_Arm_EABI;
+            }
+            #define HAS_NEON 0
+            #ifdef __ARM_NEON__
+                #undef HAS_NEON
+                #define HAS_NEON 1
+            #endif
+            if (HAS_NEON) {
+                arch |= ArchInfo_Arm_NEON;
+            }
+            return arch | detectBits() | detectEndian();
+        }
+        case (ArchInfo_Arch_PPC | ArchInfo_Bits_64): {
+            return arch | assertBits(ArchInfo_Bits_64) | assertEndian(ArchInfo_Endian_BIG);
+        }
+        case (ArchInfo_Arch_PPC | ArchInfo_Bits_32): {
+            return arch | assertBits(ArchInfo_Bits_32) | assertEndian(ArchInfo_Endian_BIG);
+        }
+        case (ArchInfo_Arch_SPARC | ArchInfo_Sparc_v9): {
+            return arch | assertBits(ArchInfo_Bits_64) | assertEndian(ArchInfo_Endian_BIG);
+        }
+        case (ArchInfo_Arch_SPARC | ArchInfo_Sparc_v8): {
+            return arch | assertBits(ArchInfo_Bits_32) | assertEndian(ArchInfo_Endian_BIG);
+        }
+        case ArchInfo_Arch_SPARC: {
+            arch |= detectBits();
+            switch (ArchInfo_getBits(arch)) {
+                case ArchInfo_Bits_32: arch |= ArchInfo_Sparc_v8; break;
+                case ArchInfo_Bits_64: arch |= ArchInfo_Sparc_v9; break;
+                default: return ArchInfo_UNKNOWN;
+            }
+            return arch | assertEndian(ArchInfo_Endian_BIG);
+        }
+        case ArchInfo_Arch_MIPS: {
+            #define IS_OABI 0
+            #if defined(_ABIO32)
+                #undef IS_OABI
+                #define IS_OABI 1
+            #endif
+            if (IS_OABI) {
+                return arch | ArchInfo_Mips_OABI | assertBits(ArchInfo_Bits_32);
+            }
+
+            #define IS_NABI 0
+            #if defined(_ABIN32)
+                #undef IS_NABI
+                #define IS_NABI 1
+            #endif
+            if (IS_NABI) {
+                if (IS_OABI) { return ArchInfo_UNKNOWN; }
+                return arch | ArchInfo_Mips_NABI | assertBits(ArchInfo_Bits_32);
+            }
+
+            // If it's not oabi or nabi and it's not mips64 then it's UNKNOWN.
+            return arch | assertBits(ArchInfo_Bits_64);
+        }
+        default: return ArchInfo_UNKNOWN;
+    }
 }
