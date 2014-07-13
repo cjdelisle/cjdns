02f19541a752e7710eef9cc651033749aa93a624
kpcyrd <git@rxv.cc>
2014-07-13 12:32:37 +0200

    Remove duplicate y/n code

--- contrib/python/cjdnsadminmaker.py
+++ contrib/python/cjdnsadminmaker.py
@@ -26,6 +26,17 @@ cjdnsadmin_path = os.path.expanduser("~/.cjdnsadmin")
 
 cjdnsadmin = {}
 
+
+def ask(question, default):
+    while True:
+        r = raw_input("%s " % question).lower() or default
+
+        if r in "yn":
+            return r == "y"
+        else:
+            print "Invalid response, please enter either y or n"
+
+
 if os.path.isfile(cjdnsadmin_path):
     validjson = False
     try:
@@ -34,23 +45,11 @@ if os.path.isfile(cjdnsadmin_path):
     except ValueError:
         pass
     if validjson:
-        while True:
-            r = raw_input("%s appears to be a valid JSON file. Update? [Y/n] " % cjdnsadmin_path)
-            if r.lower() == "n":
-                sys.exit()
-            elif r.lower() == "y" or r == "":
-                break
-            else:
-                print "Invalid response, please enter either y or n"
+        if not ask("%s appears to be a valid JSON file. Update? [Y/n]" % cjdnsadmin_path, "y"):
+            sys.exit()
     else:
-        while True:
-            r = raw_input("%s appears to be a file. Overwrite? [y/N] " % cjdnsadmin_path)
-            if r.lower() == "n" or r == "":
-                sys.exit()
-            elif r.lower() == "y":
-                break
-            else:
-                print "Invalid response, please enter either y or n"
+        if not ask("%s appears to be a file. Overwrite? [y/N]" % cjdnsadmin_path, "n"):
+            sys.exit()
 else:
     print "This script will attempt to create " + cjdnsadmin_path
 
