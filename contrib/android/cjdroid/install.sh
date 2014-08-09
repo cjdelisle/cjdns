5283d3994f99095a22a9e2adadaa9f7ebb6d8a8a
Kevin MacMartin <prurigro@gmail.com>
2014-08-09 05:51:32 -0400

    Have install check for /dev/net/tun and create if it doesn't exist

--- contrib/android/cjdroid/install.sh
+++ contrib/android/cjdroid/install.sh
@@ -1,17 +1,33 @@
 #!/system/bin/sh
 
+# Change to the script directory
 if [ ! "${0%/*}" = $(echo $0 | sed 's|^.*/||') ]; then cd "${0%/*}"; fi
 
+# Fail if user isn't root
 if [ ! `whoami` = "root" ]; then echo "Please run this script as root"; exit 1; fi
 
+# Check that /dev/net/tun exists and create it if it doesn't
+if [ ! -c /dev/net/tun ]; then
+    if [ ! -d /dev/net ]; then
+        mkdir /dev/net
+    fi
+    mknod /dev/net/tun c 10 200
+fi
+
+# Remount /system read/write
 SYSTEM=`mount | grep " /system " | grep -oe "^[^\ ]*"`
 if [ ! -e "$SYSTEM" ]; then echo "The device /system is mounted on could not be detected"; exit 1; fi
-
 mount -o rw,remount $SYSTEM /system
+
+# Copy cjdns-related-files to /system
 cp files/cjdaemon /system/bin/
 cp files/cjdctl /system/bin/
 cp files/cjdroute /system/bin/
 cp files/99cjdroute /system/etc/init.d/
+
+# Set permissions for cjdns-related-files
 chmod 755 /system/bin/cjd*
 chmod 755 /system/etc/init.d/99cjdroute
+
+# Remount /system read-only
 mount -o ro,remount $SYSTEM /system
