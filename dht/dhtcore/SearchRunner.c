c8d31c3a398bab79accb96d80a274ad02279336a
Arceliar <arceliar@gmail.com>
2014-08-11 11:30:59 +0200

    Waste less bandwidth on searches that are usually for nonexistant addresses anyway

--- dht/dhtcore/SearchRunner.c
+++ dht/dhtcore/SearchRunner.c
@@ -30,7 +30,7 @@
 
 
 /** The maximum number of requests to make before calling a search failed. */
-#define MAX_REQUESTS_PER_SEARCH 100
+#define MAX_REQUESTS_PER_SEARCH 8
 
 
 struct SearchRunner_pvt
