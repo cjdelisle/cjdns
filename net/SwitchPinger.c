a1d915002cda9dc6956b7f400db89660d2bc6f0b
Caleb James DeLisle <cjd@cjdns.fr>
2015-01-01 21:14:49 +0100

    tracking down a weird alignment issue

--- net/SwitchPinger.c
+++ net/SwitchPinger.c
@@ -217,7 +217,9 @@ static void sendPing(String* data, void* sendPingContext)
         Message_push8(msg, 0, NULL);
     }
     msg->length = 0;
+
     Message_push(msg, data->bytes, data->len, NULL);
+    Assert_true(((uintptr_t)msg->bytes % 8) || "alignment fault");
 
     if (p->pub.keyPing) {
         Message_shift(msg, Control_KeyPing_HEADER_SIZE, NULL);
