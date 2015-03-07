d1a8ae474a670facff159763ff7c27da527df253
Caleb James DeLisle <cjd@cjdns.fr>
2015-03-07 22:14:59 +0100

    missing include for windows

--- interface/tuntap/TUNInterface_win32.c
+++ interface/tuntap/TUNInterface_win32.c
@@ -12,6 +12,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "interface/Iface.h"
 #include "interface/tuntap/TUNInterface.h"
 #include "interface/tuntap/windows/TAPInterface.h"
 #include "interface/tuntap/TAPWrapper.h"
