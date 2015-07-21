648bec2c54af24799a3db2a8ebb04d863cd16443
jeffluo35 <jeffluo35@gmail.com>
2015-07-20 17:55:41 -0700

    Increase compatibility of #if
    Add in cygwin and mingw32

--- client/Configurator.c
+++ client/Configurator.c
@@ -458,7 +458,7 @@ static void security(struct Allocator* tempAlloc, List* conf, struct Log* log, s
         Dict* d = Dict_new(tempAlloc);
         Dict_putString(d, String_CONST("user"), String_CONST("nobody"), tempAlloc);
         Dict* ret = NULL;
-        #ifndef _WIN32
+        #if !defined (_WIN32) || !defined (__CYGWIN__) || !defined (__MINGW32__)
             rpcCall0(String_CONST("Security_getUser"), d, ctx, tempAlloc, &ret, true); // This causes cjdns to crash on Windows
         #endif
         uid = *Dict_getInt(ret, String_CONST("uid"));
