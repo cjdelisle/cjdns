b3524e758b4920dfe0bd6deb140ea554e73ba667
virus <virus@hostv>
2015-09-21 12:51:44 +0200

    restore warning , and add info about Seccomp_NO variable to build options

--- util/Seccomp.c
+++ util/Seccomp.c
@@ -50,6 +50,7 @@
 #if defined(si_syscall)
 # define GET_SYSCALL_NUM(si) ((si)->si_syscall)
 #else
+# warning "your libc doesn't define SIGSYS signal info! Try build witch Seccomp_NO=1"
 # define GET_SYSCALL_NUM(si) ((si)->si_value.sival_int)
 #endif
 
