4cd0930425a131b85d0442a2a359ac5c089052ae
Mada <madafoo@gmail.com>
2015-10-27 18:24:01 +0800

    Using more flag to get all peersNodes instead of random fetch

--- dht/dhtcore/RouterModule.c
+++ dht/dhtcore/RouterModule.c
@@ -268,6 +268,11 @@ static inline int sendNodes(struct NodeList* nodeList,
         struct Address addr;
         Bits_memcpyConst(&addr, &nodeList->nodes[i]->address, sizeof(struct Address));
 
+        if (i == 0 && (nodeList->size == RouterModule_K)) {
+            Dict_putInt(message->asDict, CJDHTConstants_MORE_NODES,
+                        addr.path, message->allocator);
+        }
+
         addr.path = NumberCompress_getLabelFor(addr.path, query->address->path);
 
         Address_serialize(&nodes->bytes[i * Address_SERIALIZED_SIZE], &addr);
