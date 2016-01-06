587ab7631273971ac39c55f650dee9a3829dbf00
Caleb James DeLisle <cjd@cjdns.fr>
2016-01-06 18:54:21 +0100

    Revert "Fix gcc-4.x compile Map fuzz error"
    This reverts commit 454add02f6da519254e252033687dc5b00fb3c5b.

--- util/test/Map_fuzz_test.c
+++ util/test/Map_fuzz_test.c
@@ -68,8 +68,8 @@ int main(int argc, char* argv[])
         struct Key* keys = Allocator_malloc(alloc, sizeof(struct Key) * size);
         struct Value* vals = Allocator_malloc(alloc, sizeof(struct Value) * size);
 
-        struct Key key;
-        struct Value val;
+        struct Key key = {0};
+        struct Value val = {0};
         Random_bytes(rand, (uint8_t*)&key, sizeof(key));
         Random_bytes(rand, (uint8_t*)&val, sizeof(val));
         int64_t begin = Time_hrtime();
