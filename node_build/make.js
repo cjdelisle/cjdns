4946e9ff2322e677d48c0e0856cc620cf75ae61d
epochqwert <epoch@hacking.allowed.org>
2016-01-26 19:35:40 -0600

    Tried to make the fixes for NetBSD a lot cleaner this time.

--- node_build/make.js
+++ node_build/make.js
@@ -23,7 +23,7 @@ var CanCompile = require('./CanCompile');
 var Builder = require('./builder');
 var TestRunner = require('./TestRunner');
 
-// ['linux','darwin','sunos','win32','freebsd','openbsd']
+// ['linux','darwin','sunos','win32','freebsd','openbsd','netbsd']
 var SYSTEM = process.env['SYSTEM'] || process.platform;
 var GCC = process.env['CC'];
 var CFLAGS = process.env['CFLAGS'];
@@ -234,7 +234,7 @@ Builder.configure({
 
     var dependencyDir = builder.config.buildDir + '/dependencies';
     var libuvLib = dependencyDir + '/libuv/out/Release/libuv.a';
-    if (builder.config.systemName === 'win32') {
+    if (['win32', 'netbsd'].indexOf(builder.config.systemName) >= 0) {//this might be needed for other BSDs
         libuvLib = dependencyDir + '/libuv/out/Release/obj.target/libuv.a';
     }
 
@@ -361,9 +361,10 @@ Builder.configure({
                 args.push.apply(args, env.GYP_ADDITIONAL_ARGS.split(' '));
             }
 
-            if (['freebsd', 'openbsd'].indexOf(builder.config.systemName) !== -1) {
+            if (['freebsd', 'openbsd', 'netbsd'].indexOf(builder.config.systemName) !== -1) {
                 // This platform lacks a functioning sem_open implementation, therefore...
                 args.push('--no-parallel');
+                args.push('-DOS=' + builder.config.systemName);
             }
 
             var gyp = Spawn(python, args, {env:env, stdio:'inherit'});
@@ -386,7 +387,7 @@ Builder.configure({
                 }
                 args.push('CFLAGS=' + cflags.join(' '));
 
-                var makeCommand = ['freebsd', 'openbsd'].indexOf(builder.config.systemName) >= 0 ? 'gmake' : 'make';
+                var makeCommand = ['freebsd', 'openbsd', 'netbsd'].indexOf(builder.config.systemName) >= 0 ? 'gmake' : 'make';
                 var make = Spawn(makeCommand, args, {stdio: 'inherit'});
 
                 make.on('error', function (err) {
