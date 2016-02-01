9ebb4212c0e70a43546ee498e8189266e4ac7c90
Johannes Löthberg <johannes@kyriasis.com>
2016-02-01 01:36:23 +0100

    Setuid_linux: Use version 3 of the capabilities
    _LINUX_CAPABILITY_VERSION defaults to _LINUX_CAPABILITY_VERSION_1 which
    only supports 32-bit capabilities. Use _LINUX_CAPABILITY_VERSION_3 for
    64-bit capability support.
    
    Gets rid of warning printed to the kernel ring buffer on newer kernels.
    
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- util/Setuid_linux.c
+++ util/Setuid_linux.c
@@ -52,7 +52,7 @@ void Setuid_preSetuid(struct Allocator* alloc, struct Except* eh)
     cap_user_header_t hdr = Allocator_calloc(alloc, sizeof(*hdr), 1);
     cap_user_data_t data = Allocator_calloc(alloc, sizeof(*data), 1);
 
-    hdr->version = _LINUX_CAPABILITY_VERSION;
+    hdr->version = _LINUX_CAPABILITY_VERSION_3;
     hdr->pid = 0;
     if (capGet(hdr, data)) {
         Except_throw(eh, "Error getting capabilities: [errno:%d (%s)]", errno, strerror(errno));
@@ -75,7 +75,7 @@ void Setuid_postSetuid(struct Allocator* alloc, struct Except* eh)
     cap_user_header_t hdr = Allocator_calloc(alloc, sizeof(*hdr), 1);
     cap_user_data_t data = Allocator_calloc(alloc, sizeof(*data), 1);
 
-    hdr->version = _LINUX_CAPABILITY_VERSION;
+    hdr->version = _LINUX_CAPABILITY_VERSION_3;
     hdr->pid = 0;
     if (capGet(hdr, data)) {
         Except_throw(eh, "Error getting capabilities (post-setuid): [errno:%d (%s)]",
