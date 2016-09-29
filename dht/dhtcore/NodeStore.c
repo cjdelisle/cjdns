f794ef8595005be4765b914247777a5600410006
Caleb James DeLisle <cjd@cjdns.fr>
2016-09-29 09:01:28 +0200

    Ok my fault for committing without compiling

--- dht/dhtcore/NodeStore.c
+++ dht/dhtcore/NodeStore.c
@@ -430,7 +430,7 @@ static bool findBestParent0(struct Node_Two* node, struct NodeStore_pvt* store)
 
 static void findBestParent(struct Node_Two* node, struct NodeStore_pvt* store)
 {
-    uint64_t time0 = Time_currentTimeMilliseconds(store->base);
+    uint64_t time0 = Time_currentTimeMilliseconds(store->eventBase);
     if (!findBestParent0(node, store)) { return; }
     int ret = 0;
     int cycle = 0;
@@ -444,9 +444,9 @@ static void findBestParent(struct Node_Two* node, struct NodeStore_pvt* store)
             ret |= findBestParent0(n, store);
         }
     } while (ret);
-    uint64_t time1 = Time_currentTimeMilliseconds(store->base);
+    uint64_t time1 = Time_currentTimeMilliseconds(store->eventBase);
     if ((int64_t)(time1 - time0) > 100) {
-        Log_warn(store->log, "findBestParent() took [%lld] ms", (long long) (time1 - time0));
+        Log_warn(store->logger, "findBestParent() took [%lld] ms", (long long) (time1 - time0));
     }
 }
 
