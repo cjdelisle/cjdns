71bcc8e60aa204c51db18e506d35099afae62e67
Caleb James DeLisle <cjd@cjdns.fr>
2016-11-26 03:57:16 +0100

    tinkering with bash... this could take a lot of commits

--- node_build/node.sh
+++ node_build/node.sh
@@ -96,7 +96,7 @@ checkNode() {
 getSha() {
     expected_sum='01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b'
     for shasum_tool in 'sha256sum' 'gsha256sum' 'sha256' 'shasum -a 256' 'openssl sha256'; do
-        cmdExists "$shasum_tool" || continue;
+        which "$shasum_tool" >/dev/null 2>/dev/null || continue;
         printf '\n' | $shasum_tool | $GREP_PATH -q "$expected_sum" && {
             shasum_cmd="$shasum_tool"
             return 0
