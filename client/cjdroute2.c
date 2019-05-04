ad3efd6fcc54a75eb546cbc4c4fde43d3c068846
Stuart D. Gathman <stuart@gathman.org>
2019-05-04 11:39:39 -0400

    Cjd's simplification.

--- client/cjdroute2.c
+++ client/cjdroute2.c
@@ -695,10 +695,9 @@ int main(int argc, char** argv)
     char corePipeName[64] = "client-core-";
     Random_base32(rand, (uint8_t*)corePipeName+CString_strlen(corePipeName), 31);
     String* pipePath = Dict_getStringC(config, "pipe");
-    String path;
+    String* path = String_CONST(Pipe_PATH);
     if (!pipePath) {
-        path = *String_CONST(Pipe_PATH);
-        pipePath = &path;
+        pipePath = path;
     }
     if (!Defined(win32) && access(pipePath->bytes, W_OK)) {
         Except_throw(eh, "Pipe directory not writable: [%s]",pipePath->bytes);
