09e697f4499247cdbfa0545f83fdb934e4048c95
Caleb James DeLisle <cjd@cjdns.fr>
2020-06-11 18:36:31 +0200

    Prefer building with python3 if possible

--- node_build/FindPython.js
+++ node_build/FindPython.js
@@ -22,7 +22,7 @@ var Fs = require('fs');
 // de-prioritize because the testing script accepts ANY python3 version
 // (as of this writing, we don't know what python3 versions actually work)
 // whereas we know that python2.7 is the only working python2 version.
-var PYTHONS = ["python2.7", "python2", "python3.7", "python3", "python"];
+var PYTHONS = ["python3.7", "python3", "python2.7", "python2", "python"];
 
 var SCRIPT = [
     'import sys',
