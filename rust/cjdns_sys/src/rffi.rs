b079099845905a0412befd1d4aa034fe00a2e9e0
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2022-01-13 12:42:22 -0300

    new Rffi_inet_ntop

--- rust/cjdns_sys/src/rffi.rs
+++ rust/cjdns_sys/src/rffi.rs
@@ -1,5 +1,6 @@
 use crate::cffi::{Allocator_t, Iface_t};
 use crate::external::interface::cif;
+use std::ffi::c_void;
 
 // This file is used to generate cbindings.h using cbindgen
 
@@ -26,4 +27,30 @@ pub unsafe extern "C" fn Rffi_android_create(a: *mut Allocator_t) -> Rffi_IfWrap
         external: cif::wrap(a, w.ext),
         internal: cif::wrap(a, w.int),
     }
+}
+
+/// Replaces libuv's function:
+///
+/// int uv_inet_ntop(int af, const void* src, char* dst, size_t size)
+#[no_mangle]
+pub unsafe extern "C" fn Rffi_inet_ntop(
+    is_ip6: bool,
+    addr: *const c_void,
+    dst: *mut u8,
+    dst_sz: u32,
+) -> i32 {
+    let ip_repr = if is_ip6 {
+        let addr = addr as *const [u8; 16];
+        std::net::Ipv6Addr::from(*addr).to_string()
+    } else {
+        let addr = addr as *const [u8; 4];
+        std::net::Ipv4Addr::from(*addr).to_string()
+    };
+    if ip_repr.len() >= dst_sz as usize {
+        return -1;
+    }
+    let dst = std::slice::from_raw_parts_mut(dst, dst_sz as usize);
+    dst[..ip_repr.len()].copy_from_slice(ip_repr.as_bytes());
+    dst[ip_repr.len()] = b'\0';
+    0
 }
