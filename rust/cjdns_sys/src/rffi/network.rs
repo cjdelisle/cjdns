612fb10d23a6398d83b56b7708132c20be7002b1
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2022-03-04 12:29:20 -0300

    simplify Rffi_inet_ntop with new utility

--- rust/cjdns_sys/src/rffi/network.rs
+++ rust/cjdns_sys/src/rffi/network.rs
@@ -19,23 +19,17 @@ fn n_to_ip(is_ip6: bool, addr: *const c_void) -> IpAddr {
 
 /// Convert IPv4 and IPv6 addresses from binary to text form.
 #[no_mangle]
-pub unsafe extern "C" fn Rffi_inet_ntop(
+pub extern "C" fn Rffi_inet_ntop(
     is_ip6: bool,
     addr: *const c_void,
     dst: *mut u8,
     dst_sz: u32,
 ) -> i32 {
-    let ip_repr = if is_ip6 {
-        let addr = addr as *const [u8; 16];
-        std::net::Ipv6Addr::from(*addr).to_string()
-    } else {
-        let addr = addr as *const [u8; 4];
-        std::net::Ipv4Addr::from(*addr).to_string()
-    };
+    let ip_repr = n_to_ip(is_ip6, addr).to_string();
     if ip_repr.len() >= dst_sz as usize {
         return -1;
     }
-    let dst = std::slice::from_raw_parts_mut(dst, dst_sz as usize);
+    let dst = unsafe { std::slice::from_raw_parts_mut(dst, dst_sz as usize) };
     dst[..ip_repr.len()].copy_from_slice(ip_repr.as_bytes());
     dst[ip_repr.len()] = b'\0';
     0
