20122611a5a3983dd4c67146f430533f1c61346e
William Fleurant <meshnet@protonmail.com>
2022-06-18 21:03:07 +0200

    node_build: find python2 first, and build gyp for python3.10

--- node_build/FindPython.js
+++ node_build/FindPython.js
@@ -23,7 +23,8 @@ var Fs = require('fs');
 // de-prioritize because the testing script accepts ANY python3 version
 // (as of this writing, we don't know what python3 versions actually work)
 // whereas we know that python2.7 is the only working python2 version.
-var PYTHONS = ["python3.7", "python3", "python2.7", "python2", "python"];
+var PYTHONS = ["python2", "python2.7", "python3.7", "python3.8",
+               "python3.9", "python3", "python"];
 
 var SCRIPT = [
     'import sys',
