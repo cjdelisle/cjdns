f86fa74932cc80cd91a66c73b5e863999c231c96
wiryhatbox <maturingfreebee@proton.me>
2024-07-05 18:19:33 +0300

    Make script safer with `set -e`
    More readable
    
    Quote variables

--- contrib/sh/run-cjdroute.sh
+++ contrib/sh/run-cjdroute.sh
@@ -2,6 +2,8 @@
 # A script to start cjdroute with its config as an argument
 # Maintainer: Jack L. Frost <fbt@fleshless.org>
 
+set -e
+
 # Functions
 echo() { printf '%s\n' "$*"; }
 usage() { echo "Usage: run-cjdroute [/path/to/cjdroute.conf] (/etc/cjdroute.conf by default)"; }
@@ -9,10 +11,12 @@ usage() { echo "Usage: run-cjdroute [/path/to/cjdroute.conf] (/etc/cjdroute.conf
 err() { echo "$1" >&2; }
 
 main() {
-	[ "$1" = '-h' -o "$1" = '--help' ] && { usage; return 0; }
-
-	cjdroute_config=${1:-"/etc/cjdroute.conf"}
-	exec cjdroute < $cjdroute_config
+    if [ "$1" = '-h' ] || [ "$1" = '--help' ]; then
+        usage
+        return 0
+    fi
+    cjdroute_config=${1:-"/etc/cjdroute.conf"}
+    exec cjdroute < "$cjdroute_config"
 }
 
 # DO SOMETHING
