bb78599dd444c936c4a735efc2d45ea862464daf
Caleb James DeLisle <cjd@cjdns.fr>
2024-09-18 15:59:51 +0200

    Rust/C Iface bridge: Cannot rely on drop(), must use on_free.

--- rust/cjdns_sys/src/external/interface/cif.rs
+++ rust/cjdns_sys/src/external/interface/cif.rs
@@ -1,8 +1,10 @@
-use std::sync::{Arc,Weak};
+use std::os::raw::c_void;
+use std::sync::atomic::AtomicBool;
+use std::sync::Arc;
 use anyhow::{Result,bail};
 
 use crate::cffi::{self, Allocator_t};
-use crate::rffi::allocator;
+use crate::rffi::allocator::{self, file_line};
 use crate::rtypes::RTypes_Error_t;
 use crate::external::interface::iface::{self, IfRecv, Iface, IfacePvt};
 use crate::interface::wire::message::Message;
@@ -11,12 +13,12 @@ use crate::util::identity::{Identity,from_c};
 
 struct CRecv {
     c_iface: Protected<*mut cffi::Iface>,
-    dg: Weak<()>,
+    dg: Arc<AtomicBool>,
 }
 
 impl IfRecv for CRecv {
     fn recv(&self, m: Message) -> Result<()> {
-        if self.dg.strong_count() < 1 {
+        if self.dg.load(std::sync::atomic::Ordering::Relaxed) {
             bail!("Other end has been dropped");
         }
         let c_msg = m.as_c_message();
@@ -38,7 +40,7 @@ struct CIface {
     // Additional "private" fields
     identity: Identity<Self>,
     rif: IfacePvt,
-    dg: Arc<()>,
+    dg: Arc<AtomicBool>,
 }
 
 unsafe extern "C" fn from_c(msg: *mut cffi::Message, iface_p: *mut cffi::Iface) -> *mut RTypes_Error_t {
@@ -55,14 +57,18 @@ unsafe extern "C" fn from_c(msg: *mut cffi::Message, iface_p: *mut cffi::Iface) 
     }
 }
 
+extern "C" fn on_free(iface: *mut c_void) {
+    let iface = from_c!(iface as *mut CIface);
+    iface.dg.store(true, std::sync::atomic::Ordering::Relaxed);
+}
+
 /// Create a new Iface / cffi::Iface pair which are plumbed together
 /// After calling this, you will be able to plumb your iface of choice to the Iface
 /// and then pass the cffi::Iface to C code to be plumbed to another C Iface and
 /// messages passed to the C Iface will come out in your Iface
 fn new<T: Into<String>>(alloc: *mut Allocator_t, name: T) -> (Iface, *mut cffi::Iface) {
     let (mut iface, iface_pvt) = iface::new(name);
-    let drop_guard = Arc::new(());
-    let drop_guarded = Arc::downgrade(&drop_guard);
+    let drop_guard = Arc::new(AtomicBool::new(false));
     let out = allocator::adopt(
         alloc,
         CIface {
@@ -74,11 +80,12 @@ fn new<T: Into<String>>(alloc: *mut Allocator_t, name: T) -> (Iface, *mut cffi::
             },
             identity: Default::default(),
             rif: iface_pvt,
-            dg: drop_guard,
+            dg: Arc::clone(&drop_guard),
         },
     );
+    allocator::rs(alloc).on_free(on_free, out as _, file_line!());
     let c_iface = unsafe { (&mut (*out).cif) as *mut cffi::Iface };
-    iface.set_receiver(CRecv { c_iface: Protected::new(c_iface), dg: drop_guarded });
+    iface.set_receiver(CRecv { c_iface: Protected::new(c_iface), dg: drop_guard });
     (iface, c_iface)
 }
 
